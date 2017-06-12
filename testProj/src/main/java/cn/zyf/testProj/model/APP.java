df5b504b50646c8c7462685fc7ce6390c5350cf6
zhongyf <942729783@qq.com>
2017-06-12 16:54:38 +0800

    test2

---
+++ testProj/src/main/java/cn/zyf/testProj/model/APP.java
@@ -0,0 +1,16 @@
+package cn.zyf.testProj.model;
+
+
+
+/**
+ * @author zyf
+ * @company 杭州动享互联网技术有限公司
+ * @date: 2017/6/12.
+ * @time: 16:14
+ */
+
+
+public class APP {
+
+
+}
---
+++ testProj/src/test/java/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * @author zyf
+ * @company 杭州动享互联网技术有限公司
+ * @date: 2017/6/12.
+ * @time: 16:11
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+//        public void testHello(){
+//            App app=new App();
+//            String result=app.sayHello();
+//            assertEquals("Hello",result);
+//        }
+    }
+}
